299d45fece830c3b8eb13d83ac57da978254e42e
vishwasransing <vishwasransing@gmail.com>
2023-10-18 16:41:18 +0530

    Loggers
    -Added Loggers.
    -Experimented with log levels.

--- src/main/java/com/todo/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/todo/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.todo.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +10,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 	
+	private Logger log = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("/login")
 	public String login(@RequestParam("username") String name, Model model) {
 		model.addAttribute("name", name);
+		log.info("Name: {}", name);
 		return "login";
 	}
 	
