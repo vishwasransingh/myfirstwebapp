97a26d08c11ad3cca94c0a3edbfa82388f9e4a7c
vishwasransing <vishwasransing@gmail.com>
2023-11-20 12:35:15 +0530

    H2 Setup
    -Added H2 dependencies in pom.xml
    -configured default url for h2 console in application.properties
    -Made changes in SpringSecurityConfiguration -Added SpringSecurityFilterChain.

--- src/main/java/com/todo/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/todo/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -1,14 +1,18 @@
 package com.todo.myfirstwebapp.security;
 
+import static org.springframework.security.config.Customizer.withDefaults;
+
 import java.util.function.Function;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SpringSecurityConfiguration {
@@ -36,6 +40,17 @@ public class SpringSecurityConfiguration {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+		http.formLogin(withDefaults());
+		
+		http.csrf().disable();
+		http.headers().frameOptions().disable();
+		
+		return http.build();
 	}
 
 }
