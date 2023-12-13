068c18fe01419dc848260bf1420801341a598bea
vishwasransing <vishwasransing@gmail.com>
2023-12-13 19:46:26 +0530

    Added Auto-Launch

--- src/main/java/com/todo/myfirstwebapp/MyfirstwebappApplication.java
+++ src/main/java/com/todo/myfirstwebapp/MyfirstwebappApplication.java
@@ -1,5 +1,7 @@
 package com.todo.myfirstwebapp;
 
+import java.io.IOException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,16 @@ public class MyfirstwebappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyfirstwebappApplication.class, args);
+		openWebpage("http://localhost:8080/");
 	} 
+	
+	public static void openWebpage(String url) {
+        try {
+            String command = "cmd /c start " + url;
+            Runtime.getRuntime().exec(command);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
