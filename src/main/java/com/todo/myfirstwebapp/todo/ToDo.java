221a9410d7532cccca4f24998e34327f620232d6
vishwasransing <vishwasransing@gmail.com>
2023-12-27 11:53:17 +0530

    Lombok
    Lombok integration

--- src/main/java/com/todo/myfirstwebapp/todo/ToDo.java
+++ src/main/java/com/todo/myfirstwebapp/todo/ToDo.java
@@ -7,7 +7,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class ToDo {
 
@@ -15,74 +23,14 @@ public class ToDo {
 	@GeneratedValue
 	private int id;
 
-	
 	private String username;
 
-	@Column(name="description")
+	@Column(name = "description")
 	@Size(min = 5, message = "Description should be at least 5 characters long.")
 	private String desc;
 
 	private LocalDate targetDate;
 
 	private Boolean done;
-	
-	ToDo(){
-		
-	}
-
-	public ToDo(int id, String username, String desc, LocalDate targetDate, Boolean done) {
-		super();
-		this.id = id;
-		this.username = username;
-		this.desc = desc;
-		this.targetDate = targetDate;
-		this.done = done;
-	}
-
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getDesc() {
-		return desc;
-	}
-
-	public void setDesc(String desc) {
-		this.desc = desc;
-	}
-
-	public LocalDate getTargetDate() {
-		return targetDate;
-	}
-
-	public void setTargetDate(LocalDate targetDate) {
-		this.targetDate = targetDate;
-	}
-
-	public Boolean getDone() {
-		return done;
-	}
-
-	public void setDone(Boolean done) {
-		this.done = done;
-	}
-
-	@Override
-	public String toString() {
-		return "ToDo [id=" + id + ", username=" + username + ", desc=" + desc + ", targetDate=" + targetDate + ", done="
-				+ done + "]";
-	}
 
 }
